fab37454f8fb5c5b281b52419cb3a2141ca8af68
ilanuk <ilanuk@yahoo.com>
2017-07-29 21:20:51 -0700

    made the ajax call as per the spec

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,36 @@
 $(function() {
-
+// The above call is short of the expanded form of the call such as $(document).ready(function(){
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/ilanuk.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+	var courses = response.courses.completed;
+	var $badges = $('#badges');
+	var i;
+	courses.forEach(function(course) {
+ 		$div = $('<div />',{
+			'class': 'course'
+		}).appendTo($badges);
+		$('<h3 />', {
+			text: course.title
+		}).appendTo($div);
+		$('<img />', {
+			src: course.badge
+		}).appendTo($div);
+		$('<a />', {
+			'class': 'btn btn-primary',
+			target : '_blank',
+			href: course.url,
+			text : 'See Course'
+		}).appendTo($div);
+	});	
+    },
+    error: function() {
+	alert('Error: '+errorType + ' with Message: '+ errorMessage);	
+    },
+    timeout: 3000
+  });
 
 });
